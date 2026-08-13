a3f028d89ba66806132e314b3c77b462c30b0b62
TulioG_Projects <tuliog.projects@gmail.com>
2026-08-13 18:56:22 +0800

    setup game loop

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock_time = pygame.time.Clock()
+    dt = 0.0
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        for event in pygame.event.get():
+            pass
+
+        screen.fill(color="black")
+
+        dt = clock_time.tick(60) / 1000
+
+        # print(f"delta-time: {dt}")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
