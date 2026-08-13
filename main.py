2f46779e8f339d524dce5ea001f4e018500f3004
TulioG_Projects <tuliog.projects@gmail.com>
2026-08-13 23:11:14 +0800

    add asteroid and shot collision

--- main.py
+++ main.py
@@ -61,6 +61,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if ast.collides_with(shot):
+                    log_event("asteroid_shot")
+                    ast.kill()
+                    shot.kill()
 
         pygame.display.flip()
 
